5433de31100ea58b194b0b36a0a1f3f9b010a0fe
Veronica Romo <veronicalizethromo@gmail.com>
2021-08-05 21:02:50 -0500

    Cambios en navbar y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { FooterComponent } from './Shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
