bcce367135c76d8ca2c9460105754e504dc145c2
veroromo06 <veronicalizethromo@gmail.com>
2021-08-08 15:28:35 -0500

    Página de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Shared/navbar/navbar.component';
 import { InicioComponent } from './Pages/inicio/inicio.component';
 import { FooterComponent } from './Shared/footer/footer.component';
+import { AboutComponent } from './Pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     InicioComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
