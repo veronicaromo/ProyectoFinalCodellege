54c4731084b4503704040c046caf13ef5a1bdacc
veroromo06 <veronicalizethromo@gmail.com>
2021-08-09 16:44:18 -0500

    Página inicio y about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Pages/about/about.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'inicio'} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
