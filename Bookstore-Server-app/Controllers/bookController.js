56579a037fec795c701843a48ae032ac3d872c61
AbigailLopezRivera <75084113+AbigailLopezRivera@users.noreply.github.com>
2021-08-11 20:22:50 -0500

    Parte del Server

---
+++ Bookstore-Server-app/Controllers/bookController.js
@@ -0,0 +1,123 @@
+//exporta de modulos
+var Book = require("../models/bookModel").Book;
+
+//Exporta una funación que envia un mensaje
+exports.root = function(req, res) {
+    res.send("Bookstore API");
+}
+
+exports.index = function(req, res) {
+    //El modelo (book) simplifica la conexión a la bd
+    Book.find({}, //Función de Moongose
+        function(err, docs) {
+            if (!err) { //si no existe un error trae el json de docs
+                res.status(200).json({ books: docs });
+            } else { //500 error del lado del servidor
+                res.status(500).json({
+                    message: "Error en la bd" +
+                        err
+                });
+            }
+        });
+}
+
+//Muestra los libros por id
+exports.show = function(req, res) {
+    var id = req.params.id; //Muestra el id de cada libro que se quiera
+    Book.findById(id, function(err, doc) {
+        if (!err && doc) { //Si se encuentra el libro y no existe el error, se muestra el json del doc
+            res.status(200).json(doc);
+        } else if (err) { //Error del servidor
+            res.status(500).json({ message: "Error loading book" + err });
+        } else { //Error porque no se encontró el libro
+            res.status(404).json({ message: "Book not found" });
+        }
+    });
+}
+
+exports.create = function(req, res) {
+    var title = req.body.title;
+    var author = req.body.author;
+    var literaryGenres = req.body.literaryGenres;
+    var year = req.body.year;
+    var pages = req.body.pages;
+    var description = req.body.description;
+    var price = req.body.price;
+    var units = req.body.units;
+
+    Book.findOne({ title: { $regex: new RegExp(title, "i") } }, function(err, doc) { //regex es una expresión regular isensitivo
+        if (!err && !doc) { //si  no hay error y no se encuentra el documento, se crea el objeto libro
+            var newBook = new Book(); // se crea un nuevi objeto de acuerdo al esquema que se definió
+            newBook.title = title;
+            newBook.author = author;
+            newBook.literaryGenres = literaryGenres;
+            newBook.year = year;
+            newBook.pages = pages;
+            newBook.description = description;
+            newBook.price = price;
+            newBook.units = units;
+            newBook.save(function(err) {
+                if (!err) { // En caso de que no exista ningun erro
+                    res.status(200).json({ message: "Book created with name: " + newBook.title });
+                } else {
+                    res.status(500).json({ message: "Could not create book, Error: " + err });
+                }
+            });
+        } else if (!err) {
+            res.status(400).json({ message: "Book with that name already exists" });
+        } else {
+            res.status(500).json({ message: err });
+        }
+    });
+}
+
+exports.update = function(req, res) {
+    var id = req.body.id;
+    var title = req.body.title;
+    var author = req.body.author;
+    var literaryGenres = req.body.literaryGenres;
+    var year = req.body.year;
+    var pages = req.body.pages;
+    var description = req.body.description;
+    var price = req.body.price;
+    var units = req.body.units;
+
+    Book.findById(id, function(err, doc) {
+        if (!err && doc) { // se encuentra el documento para actualizar
+            doc.title = title;
+            doc.author = author;
+            doc.literaryGenres = literaryGenres;
+            doc.year = year;
+            doc.pages = pages;
+            doc.description = description;
+            doc.price = price;
+            doc.units = units;
+            doc.save(function(err) {
+                if (!err) { //si no hay error, se actualiza el documento
+                    res.status(200).json({ message: "Book updated: " + title });
+                } else {
+                    res.status(500).json({ message: "Could not ypdate book " + err });
+                }
+            });
+        } else if (!err) {
+            res.status(404).json({ message: "Could not find book" });
+        } else {
+            res.status(500).json({ message: "Could not update book " + err });
+        }
+    });
+}
+
+exports.delete = function(req, res) {
+    var id = req.body.id;
+    var title = req.body.title;
+    Book.findById(id, function(err, doc) {
+        if (!err && doc) {
+            doc.remove();
+            res.status(200).json({ message: "Book removed." });
+        } else if (!err) {
+            res.status(404).json({ message: "Could not find book" });
+        } else {
+            res.status(500).json({ message: "Could not delete book " + err });
+        }
+    });
+}
---
+++ Bookstore-Server-app/index.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    http = require('http'),
+    bodyParser = require('body-parser'),
+    books = require('./Controllers/bookController'),
+    mongoose = require('mongoose');
+
+//MongoDB Conexión
+mongoose.connect('mongodb://localhost:27017/Bookstore', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+
+}).then(() => {
+    console.log("Connected to BD");
+}).catch(err => {
+    console.log("The connection to BD coul not be stablised");
+    process.exit();
+});
+
+
+var server = express();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.set('port', process.env.PORT || 3000);
+
+server.get('/', books.root);
+server.get('/books', books.index); //obtiene  la info todos los documentos de los libros
+server.get('/books/:id', books.show); //Obtiene la info de un libro en especifico por el id
+server.post('/books', books.create); //Crea un nuevo documuento de libro
+server.put('/books', books.update); //Edita la info del documento del libro
+server.delete('/books', books.delete); //elimina un libro
+
+
+http.createServer(server).listen(server.get('port'), function() {
+    console.log("Express server listening on port %s.", server.get('port'));
+});
